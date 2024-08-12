ff586267225e9a951833337d3a504862b5e65033
independentdevs <inddevs157@gmail.com>
2024-08-12 22:17:14 +0200

    add correct app files

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+/**
+ * The SignIn component renders a simple "SignIn" text.
+ * This is coded up to the file and folder structure part of video.
+ * @returns {JSX.Element} A div element containing the text "SignIn".
+ */
+const SignIn = () => {
+    return (
+        <div> SignIn </div>
+    )
+}
+
+export default SignIn
+
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/**
+ * The SignUp component renders a simple "SignUp" text.
+ * This is coded up to the file and folder structure part of video.
+ * @returns {JSX.Element} A div element containing the text "SignUp".
+ */
+const SignUp = () => {
+    return (
+        <div>SignUp</div>
+    )
+}
+
+export default SignUp
