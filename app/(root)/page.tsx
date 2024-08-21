5672f8ea8142068e2bcdf4d8bf2eebaa43612481
independentdevs <inddevs157@gmail.com>
2024-08-21 15:26:50 +0200

    Implement HeaderBox component & Header to Dashboard (Home)

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
 const Dashboard = () => {
+    const loggedIn = { firstName: 'Kenneth'};
+    
     return (
-        <div>Dashboard</div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox 
+                      type="greeting"
+                      title="Welcome"
+                      user= {loggedIn?.firstName || 'Guest'}
+                      subtext="Access and manage your account and transactions efficiently."
+                    />
+                </header>
+            </div>
+        </section>
     )
 }
 
